07e200bab0c757aaa62add19d2da94a5c7ad148a
Emiliano <e.acevedo.felip@gmail.com>
2023-11-02 23:58:07 -0300

    set webmanifest and sw for pwa xp

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'my-astro-pwa-cache';
+const urlsToCache = [
+  '/astro-test/favicon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
